93f8129ea321eeeab239ecc194bd330baee94972
bridgez <efool@qq.com>
2023-11-05 20:35:40 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,9 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   default = "eastus"
 }
+
+variable "admin_password" {
+  type    = string
+  description = "The admin password for the virtual machine"
+}
+
