b56e6b8fe40f311ac6242e35bf27e3d1d379a303
bridgez <efool@qq.com>
2023-11-05 11:36:07 +0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "resource_group_id" {
   value = azurerm_resource_group.resourceGroup.id
 }
-output "public_ip_address" {
-  value = azurerm_public_ip.publicip
-}
+#output "public_ip_address" {
+#  value = azurerm_public_ip.publicip
+#}
 
