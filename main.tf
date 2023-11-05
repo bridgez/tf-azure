ca0e138d7152846dcf41a6bae2d312bf8dd81771
bridgez <efool@qq.com>
2023-11-05 18:39:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,208 +5,140 @@ terraform {
       name = "tf-azure"      
     }
   }
-
-#  required_version = ">=0.12"
-  
-#  required_providers {
-#    azurerm = {
-#      source = "hashicorp/azurerm"
-#      version = "~>2.0"
-#    }
-#  }
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-#provider "azurerm" {
-#  features {}
-#}
-
-resource "azurerm_resource_group" "vmss" {
- name     = var.resource_group_name
- location = var.location
- tags     = var.tags
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
 }
 
-resource "random_string" "fqdn" {
- length  = 6
- special = false
- upper   = false
- numeric  = false
+resource "random_pet" "azurerm_virtual_network_name" {
+  prefix = "vnet"
 }
 
-resource "azurerm_virtual_network" "vmss" {
- name                = "vmss-vnet"
- address_space       = ["10.0.0.0/16"]
- location            = var.location
- resource_group_name = azurerm_resource_group.vmss.name
- tags                = var.tags
+resource "azurerm_virtual_network" "test" {
+  name                = random_pet.azurerm_virtual_network_name.id
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "vmss" {
- name                 = "vmss-subnet"
- resource_group_name  = azurerm_resource_group.vmss.name
- virtual_network_name = azurerm_virtual_network.vmss.name
- address_prefixes       = ["10.0.2.0/24"]
+resource "random_pet" "azurerm_subnet_name" {
+  prefix = "sub"
 }
 
-resource "azurerm_public_ip" "vmss" {
- name                         = "vmss-public-ip"
- location                     = var.location
- resource_group_name          = azurerm_resource_group.vmss.name
- allocation_method            = "Static"
- domain_name_label            = random_string.fqdn.result
- tags                         = var.tags
+resource "azurerm_subnet" "test" {
+  name                 = random_pet.azurerm_subnet_name.id
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_lb" "vmss" {
- name                = "vmss-lb"
- location            = var.location
- resource_group_name = azurerm_resource_group.vmss.name
-
- frontend_ip_configuration {
-   name                 = "PublicIPAddress"
-   public_ip_address_id = azurerm_public_ip.vmss.id
- }
-
- tags = var.tags
+resource "azurerm_public_ip" "test" {
+  name                = "publicIPForLB"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
 }
 
-resource "azurerm_lb_backend_address_pool" "bpepool" {
- loadbalancer_id     = azurerm_lb.vmss.id
- name                = "BackEndAddressPool"
+resource "azurerm_lb" "test" {
+  name                = "loadBalancer"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  frontend_ip_configuration {
+    name                 = "publicIPAddress"
+    public_ip_address_id = azurerm_public_ip.test.id
+  }
 }
 
-resource "azurerm_lb_probe" "vmss" {
-# resource_group_name = azurerm_resource_group.vmss.name
- loadbalancer_id     = azurerm_lb.vmss.id
- name                = "ssh-running-probe"
- port                = var.application_port
+resource "azurerm_lb_backend_address_pool" "test" {
+  loadbalancer_id = azurerm_lb.test.id
+  name            = "BackEndAddressPool"
 }
 
-resource "azurerm_lb_rule" "lbnatrule" {
-#   resource_group_name            = azurerm_resource_group.vmss.name
-   loadbalancer_id                = azurerm_lb.vmss.id
-   name                           = "http"
-   protocol                       = "Tcp"
-   frontend_port                  = var.application_port
-   backend_port                   = var.application_port
-#   backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
-   frontend_ip_configuration_name = "PublicIPAddress"
-   probe_id                       = azurerm_lb_probe.vmss.id
+resource "azurerm_network_interface" "test" {
+  count               = 2
+  name                = "acctni${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "testConfiguration"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
-resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
- name                = "vmscaleset"
- location            = var.location
- resource_group_name = azurerm_resource_group.vmss.name
- upgrade_policy_mode = "Manual"
-
- sku {
-   name     = "Standard_DS1_v2"
-   tier     = "Standard"
-   capacity = 2
- }
-
- storage_profile_image_reference {
-   publisher = "Canonical"
-   offer     = "UbuntuServer"
-   sku       = "16.04-LTS"
-   version   = "latest"
- }
-
- storage_profile_os_disk {
-   name              = ""
-   caching           = "ReadWrite"
-   create_option     = "FromImage"
-   managed_disk_type = "Standard_LRS"
- }
-
- storage_profile_data_disk {
-   lun          = 0
-   caching        = "ReadWrite"
-   create_option  = "Empty"
-   disk_size_gb   = 10
- }
-
- os_profile {
-   computer_name_prefix = "vmlab"
-   admin_username       = var.admin_user
-   admin_password       = var.admin_password
-   custom_data          = file("web.conf")
- }
-
- os_profile_linux_config {
-   disable_password_authentication = false
- }
-
- network_profile {
-   name    = "terraformnetworkprofile"
-   primary = true
-
-   ip_configuration {
-     name                                   = "IPConfiguration"
-     subnet_id                              = azurerm_subnet.vmss.id
-     load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
-     primary = true
-   }
- }
-
- tags = var.tags
+resource "azurerm_availability_set" "avset" {
+  name                         = "avset"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
 }
 
-resource "azurerm_public_ip" "jumpbox" {
- name                         = "jumpbox-public-ip"
- location                     = var.location
- resource_group_name          = azurerm_resource_group.vmss.name
- allocation_method            = "Static"
- domain_name_label            = "${random_string.fqdn.result}-ssh"
- tags                         = var.tags
+resource "random_pet" "azurerm_linux_virtual_machine_name" {
+  prefix = "vm"
 }
 
-resource "azurerm_network_interface" "jumpbox" {
- name                = "jumpbox-nic"
- location            = var.location
- resource_group_name = azurerm_resource_group.vmss.name
+resource "azurerm_linux_virtual_machine" "test" {
+  count                 = 2
+  name                  = "${random_pet.azurerm_linux_virtual_machine_name.id}${count.index}"
+  location              = azurerm_resource_group.rg.location
+  availability_set_id   = azurerm_availability_set.avset.id
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.test[count.index].id]
+  size                  = "Standard_DS1_v2"
 
- ip_configuration {
-   name                          = "IPConfiguration"
-   subnet_id                     = azurerm_subnet.vmss.id
-   private_ip_address_allocation = "dynamic"
-   public_ip_address_id          = azurerm_public_ip.jumpbox.id
- }
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
 
- tags = var.tags
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  admin_ssh_key {
+    username   = var.username
+    public_key = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "myosdisk${count.index}"
+  }
+
+  computer_name  = "hostname"
+  admin_username = var.username
 }
 
-resource "azurerm_virtual_machine" "jumpbox" {
- name                  = "jumpbox"
- location              = var.location
-# resource_group_name   = azurerm_resource_group.vmss.name
- network_interface_ids = [azurerm_network_interface.jumpbox.id]
- vm_size               = "Standard_DS1_v2"
+resource "azurerm_managed_disk" "test" {
+  count                = 2
+  name                 = "datadisk_existing_${count.index}"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1024"
+}
 
- storage_image_reference {
-   publisher = "Canonical"
-   offer     = "UbuntuServer"
-   sku       = "16.04-LTS"
-   version   = "latest"
- }
+resource "azurerm_virtual_machine_data_disk_attachment" "test" {
+  count              = 2
+  managed_disk_id    = azurerm_managed_disk.test[count.index].id
+  virtual_machine_id = azurerm_linux_virtual_machine.test[count.index].id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
 
- storage_os_disk {
-   name              = "jumpbox-osdisk"
-   caching           = "ReadWrite"
-   create_option     = "FromImage"
-   managed_disk_type = "Standard_LRS"
- }
 
- os_profile {
-   computer_name  = "jumpbox"
-   admin_username = var.admin_user
-   admin_password = var.admin_password
- }
-
- os_profile_linux_config {
-   disable_password_authentication = false
- }
-
- tags = var.tags
 }
