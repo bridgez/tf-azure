092a445fc3c6844aff306054392a1ca70fb14c8a
bridgez <efool@qq.com>
2023-11-05 18:40:10 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,17 @@
-variable "resource_group_name" {
-   description = "Name of the resource group in which the resources will be created"
-   default     = "myResourceGroup"
+variable "resource_group_location" {
+  type        = string
+  description = "Location for all resources."
+  default     = "eastus"
 }
 
-variable "location" {
-   default = "eastus"
-   description = "Location where resources will be created"
+variable "resource_group_name_prefix" {
+  type        = string
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+  default     = "rg"
 }
 
-variable "tags" {
-   description = "Map of the tags to use for the resources that are deployed"
-   type        = map(string)
-   default = {
-      environment = "codelab"
-   }
-}
-
-variable "application_port" {
-   description = "Port that you want to expose to the external load balancer"
-   default     = 80
-}
-
-variable "admin_user" {
-   description = "User name to use as the admin account on the VMs that will be part of the VM scale set"
-   default     = "azureuser"
-}
-
-variable "admin_password" {
-   description = "Default password for admin account"
+variable "username" {
+  type        = string
+  description = "The username for the local account that will be created on the new VM."
+  default     = "azureadmin"
 }
