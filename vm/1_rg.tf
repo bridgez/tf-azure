04c21150c2c6b483fcf249fd96b199c214d0e36d
bridgehash <efool@qq.com>
2023-11-06 20:51:42 +0800

    vari

--- vm/1_rg.tf
+++ vm/1_rg.tf
@@ -34,6 +34,10 @@ variable "ARM_TENANT_ID" {
 variable "admin_password" {
   description = "admin_password"
 }
+variable "azurerm_resource_group" {
+  description = "azurerm_resource_group"
+}
+
 # The provider block configures the specified provider, in this case azurerm. A provider is a plugin that Terraform uses to create and manage your resources.
 provider "azurerm" {
   features {} #  At least 1 "features" blocks are required. 
