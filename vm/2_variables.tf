28f255a14d820f26a4e5166789784d5dbb79df7c
bridgez <efool@qq.com>
2023-11-06 21:04:04 +0800

    Update 2_variables.tf

--- vm/2_variables.tf
+++ vm/2_variables.tf
@@ -3,8 +3,10 @@
 
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default = "bridge1106"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "east asia"
 }
