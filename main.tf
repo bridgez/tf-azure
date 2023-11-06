41c12ffe778a618127b2b3d98e9f602fec164bb7
bridgehash <efool@qq.com>
2023-11-06 16:57:49 +0800

    without -a

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+variable "ARM_CLIENT_ID" {
+  description = "ARM_CLIENT_ID"
+}
+variable "ARM_CLIENT_SECRET" {
+  description = "ARM_CLIENT_SECRET"
+}
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "ARM_SUBSCRIPTION_ID"
+}
+
+variable "ARM_TENANT_ID" {
+  description = "ARM_TENANT_ID"
+}
+
+variable "admin_password" {
+  description = "admin_password"
+}
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
