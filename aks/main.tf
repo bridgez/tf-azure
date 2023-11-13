0f128610d5250d85f76ed61e03978da33bd7ad4c
bridgez <efool@qq.com>
2023-11-13 22:00:26 +0800

    Create main.tf

---
+++ aks/main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "aks" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+}
