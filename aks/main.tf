6da4f831be3a7f90039f3a45b2b20526589dfe8f
bridgez <efool@qq.com>
2023-11-14 19:25:36 +0800

    Update main.tf

--- aks/main.tf
+++ aks/main.tf
@@ -1,21 +1,28 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "aks" {
-  name     = "myResourceGroup"
-  location = "East US"
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-k8s-resources"
+  location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "myAKSCluster"
-  location            = azurerm_resource_group.aks.location
-  resource_group_name = azurerm_resource_group.aks.name
-  dns_prefix          = "myaksdns"
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "${var.prefix}-k8s"
 
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
