de4809a33c2f4f877180acd86877b975c4df77b9
bridgez <efool@qq.com>
2023-11-15 20:55:09 +0800

    Update variables.tf

--- aks/variables.tf
+++ aks/variables.tf
@@ -10,3 +10,13 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
   default = "East US"
 }
+
+variable "username" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "azuser"
+}
+
+variable "password" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "Passw0rd!23"
+}
