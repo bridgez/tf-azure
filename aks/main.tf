b895a599a8ad73b5d73506ef7a19812989b285cc
bridgez <efool@qq.com>
2023-11-20 21:28:26 +0800

    count added.

--- aks/main.tf
+++ aks/main.tf
@@ -9,15 +9,13 @@ resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-k8s-resources"
   location = var.location
 }
-variable "aks_count" {
-  default = 2  # 你想要的 AKS 集群数量
-}
+
 resource "azurerm_kubernetes_cluster" "example" {
-  count               = var.aks_count
-  name                = "${var.prefix}-k8s-${count.index + 1}"  # "${var.prefix}-k8s"
+  count               = 2  # 可以根据需要更改集群数量
+  name                = "${var.prefix}-k8s-${count.index + 1}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "${var.prefix}-k8s-${count.index + 1}" # "${var.prefix}-k8s"
+  dns_prefix          = "${var.prefix}-k8s-${count.index + 1}"
 
   default_node_pool {
     name       = "default"
