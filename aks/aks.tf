21ae631af0ad98cd90cc7b56c625737f2d657a50
bridgez <efool@qq.com>
2023-11-21 23:38:47 +0800

    double

--- aks/aks.tf
+++ aks/aks.tf
@@ -9,29 +9,26 @@ resource "azurerm_resource_group" "example" {
 
 variable "node_pools" {
   type = list(object({
-    name              = string
-    vm_size           = string
-    node_count        = number
-    enable_auto_scaling  = bool
-    min_count         = number
-    max_count         = number
+    name       = string
+    node_count = number
+    vm_size    = string
   }))
   default = [
     {
-      name               = "default"
-      vm_size            = "Standard_DS2_v2"
-      node_count         = 1
+      name       = "default"
+      node_count = 1
       enable_auto_scaling = true
       min_count          = 1
       max_count          = 3
+      vm_size    = "Standard_DS2_v2"
     },
     {
-      name               = "advanced"
-      vm_size            = "Standard_DS3_v2"
-      node_count         = 1
+      name       = "busy"
+      node_count = 2
       enable_auto_scaling = false
       min_count          = 1
       max_count          = 3
+      vm_size    = "Standard_DS2_v2"
     },
   ]
 }
@@ -45,26 +42,13 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
     name       = var.node_pools[count.index].name
-    vm_size    = var.node_pools[count.index].vm_size
     node_count = var.node_pools[count.index].node_count
-
-    enable_auto_scaling = var.node_pools[count.index].enable_auto_scaling
-    min_count          = var.node_pools[count.index].min_count
-    max_count          = var.node_pools[count.index].max_count
-  }
-
-  advanced_node_pool {
-    name       = "advanced"
-    vm_size    = "Standard_DS3_v2"
-    node_count = 1
-    enable_auto_scaling = false
-    min_count          = 1
-    max_count          = 3
+    vm_size    = var.node_pools[count.index].vm_size
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  kubernetes_version = "1.27.7"
+  kubernetes_version = "1.27.7"  # 指定的 Kubernetes 版本
 }
