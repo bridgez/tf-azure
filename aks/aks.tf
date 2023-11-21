e9386d119cae74fbec6f84ca7331929b4f06a6e3
bridgez <efool@qq.com>
2023-11-21 22:45:45 +0800

    Update aks.tf

--- aks/aks.tf
+++ aks/aks.tf
@@ -9,48 +9,39 @@ resource "azurerm_resource_group" "example" {
 
 variable "node_pools" {
   type = list(object({
-    name                = string
-    node_count          = number
-    vm_size             = string
-    enable_auto_scaling = bool
-    min_count           = number
-    max_count           = number
+    name       = string
+    vm_size    = string
   }))
   default = [
     {
-      name                = "default"
-      node_count          = 1
-      vm_size             = "Standard_DS2_v2"
-      enable_auto_scaling = true
-      min_count           = 1
-      max_count           = 5
+      name    = "default"
+      vm_size = "Standard_DS2_v2"
     },
     {
-      name       = "busy"
-      node_count = 2
-      vm_size    = "Standard_DS2_v2"
-      enable_auto_scaling = true
-      min_count           = 1
-      max_count           = 5
+      name    = "busy"
+      vm_size = "Standard_DS2_v2"
     },
   ]
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  count               = length(var.node_pools)
+  count               = 2
   name                = "${var.prefix}-k8s-${count.index + 1}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = "${var.prefix}-k8s-${count.index + 1}"
 
-  default_node_pool {
-    name       = var.node_pools[count.index].name
-    node_count = var.node_pools[count.index].node_count
-    vm_size    = var.node_pools[count.index].vm_size
+  dynamic "default_node_pool" {
+    for_each = var.node_pools
 
-    enable_auto_scaling = var.node_pools[count.index].enable_auto_scaling
-    min_count           = var.node_pools[count.index].min_count
-    max_count           = var.node_pools[count.index].max_count
+    content {
+      name       = default_node_pool.value.name
+      vm_size    = default_node_pool.value.vm_size
+      node_count = 1  # 最小节点数
+      enable_auto_scaling = true
+      min_count           = 1
+      max_count           = 5  # 最大节点数
+    }
   }
 
   identity {
