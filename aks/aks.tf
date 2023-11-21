d8f2a42edb3cbb95d5b4018e25f9673f241dd828
bridgez <efool@qq.com>
2023-11-21 23:29:59 +0800

    Update aks.tf

--- aks/aks.tf
+++ aks/aks.tf
@@ -8,49 +8,58 @@ resource "azurerm_resource_group" "example" {
 }
 
 variable "node_pools" {
-  type = map(object({
-    vm_size             = string
-    node_count          = number
-    enable_auto_scaling = bool
-    min_count           = number
-    max_count           = number
+  type = list(object({
+    name              = string
+    vm_size           = string
+    node_count        = number
+    enable_auto_scaling  = bool
+    min_count         = number
+    max_count         = number
   }))
-  default = {
-    default = {
-      vm_size             = "Standard_DS2_v2"
-      node_count          = 1
+  default = [
+    {
+      name               = "default"
+      vm_size            = "Standard_DS2_v2"
+      node_count         = 1
       enable_auto_scaling = true
-      min_count           = 1
-      max_count           = 3
-    }
-    advanced = {
-      vm_size             = "Standard_DS3_v2"
-      node_count          = 1
-      enable_auto_scaling = false  # 手动伸缩
-      min_count           = 1
-      max_count           = 3
-    }
-  }
+      min_count          = 1
+      max_count          = 3
+    },
+    {
+      name               = "advanced"
+      vm_size            = "Standard_DS3_v2"
+      node_count         = 1
+      enable_auto_scaling = false
+      min_count          = 1
+      max_count          = 3
+    },
+  ]
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  for_each            = var.node_pools
-  name                = "${var.prefix}-k8s-${each.key}"
+  count               = length(var.node_pools)
+  name                = "${var.prefix}-k8s-${count.index + 1}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "${var.prefix}-k8s-${each.key}"
+  dns_prefix          = "${var.prefix}-k8s-${count.index + 1}"
 
-  dynamic "node_pool" {
-    for_each = [1]  # 固定值 [1] 表示每个节点池都会被创建
-    content {
-      name       = each.key
-      vm_size    = each.value.vm_size
-      node_count = each.value.node_count
+  default_node_pool {
+    name       = var.node_pools[count.index].name
+    vm_size    = var.node_pools[count.index].vm_size
+    node_count = var.node_pools[count.index].node_count
 
-      enable_auto_scaling = each.value.enable_auto_scaling
-      min_count           = each.value.min_count
-      max_count           = each.value.max_count
-    }
+    enable_auto_scaling = var.node_pools[count.index].enable_auto_scaling
+    min_count          = var.node_pools[count.index].min_count
+    max_count          = var.node_pools[count.index].max_count
+  }
+
+  advanced_node_pool {
+    name       = "advanced"
+    vm_size    = "Standard_DS3_v2"
+    node_count = 1
+    enable_auto_scaling = false
+    min_count          = 1
+    max_count          = 3
   }
 
   identity {
