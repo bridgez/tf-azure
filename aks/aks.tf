93daee1f52038890cc9fc09a8e1d4a2608b3006f
bridgez <efool@qq.com>
2023-11-21 22:06:41 +0800

    Update aks.tf

--- aks/aks.tf
+++ aks/aks.tf
@@ -7,32 +7,42 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
+variable "node_pools" {
+  type = list(object({
+    name       = string
+    node_count = number
+    vm_size    = string
+  }))
+  default = [
+    {
+      name       = "default"
+      node_count = 1
+      vm_size    = "Standard_DS2_v2"
+    },
+    {
+      name       = "busy"
+      node_count = 5
+      vm_size    = "Standard_DS2_v2"
+    },
+  ]
+}
+
 resource "azurerm_kubernetes_cluster" "example" {
-  count               = 2  # 可以根据需要更改集群数量
+  count               = length(var.node_pools)
   name                = "${var.prefix}-k8s-${count.index + 1}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = "${var.prefix}-k8s-${count.index + 1}"
 
-  tags = {
-    env = "cdt"
-    # 其他标签键值对
-  }
-
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  default_node_pool {
-    name       = "busy"
-    node_count = 5
-    vm_size    = "Standard_DS2_v2"
+    name       = var.node_pools[count.index].name
+    node_count = var.node_pools[count.index].node_count
+    vm_size    = var.node_pools[count.index].vm_size
   }
 
   identity {
     type = "SystemAssigned"
   }
+
   kubernetes_version = "1.27.7"  # 指定的 Kubernetes 版本
 }
