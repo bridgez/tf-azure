61e256c26f020a7beffb365e69572db5ab5b54bc
bridgez <efool@qq.com>
2023-11-27 20:41:24 +0800

    Update aks.tf

--- aks/aks.tf
+++ aks/aks.tf
@@ -41,8 +41,6 @@ resource "azurerm_kubernetes_cluster" "example" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    min_count  = 1
-    max_count  = 3
     vm_size    = "Standard_DS2_v2"
   }
 
